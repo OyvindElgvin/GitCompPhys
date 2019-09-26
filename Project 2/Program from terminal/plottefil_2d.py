8fa2df993a829d3b6d1ff4758f597ecb162da4ea
Øyvind <stoyvind@gmail.com>
2019-09-26 14:53:49 +0200

    added files

--- Project 2/Program from terminal/plottefil_2d.py
+++ Project 2/Program from terminal/plottefil_2d.py
@@ -5,7 +5,7 @@ import numpy as np
 
 cols = []
 
-f = open("2bA_eigvalues","r")
+f = open("2dA_eigvalues","r")
 f.readline()
 f.readline()
 for line in f:
@@ -18,11 +18,12 @@ for line in f:
 
 
 
-R = np.loadtxt("2bR", skiprows=2)
+R = np.loadtxt("2dR", skiprows=2)
 #print(R)
 
 GS_index = np.argmin(np.array(cols))
 
 GS_vec = R[:, GS_index]
+print("jacobi 2d lowest eigenvalu:")
 print(cols[GS_index])
-print(GS_index, GS_vec)
+#print(GS_index, GS_vec)
